1a0b17ebdf4c17d82f196196021be37a767ef9c9
Het-Dhorajiya-0305 <hetdhorajiya03@gmail.com>
2024-06-25 12:11:15 +0530

    calculator is ready

--- calculater.js
+++ calculater.js
@@ -7,14 +7,14 @@ const eqaul = document.querySelector(".ans");
 
 let s1 = "";
 let s3 = "";
-let s2 = [];
-let ope1 = [];
+let numbers = [];  
+let operators = [];
 let finalAns = 0;
 
 eqaul.addEventListener("click", () => {
-    s2.push(s3);
-    console.log(ope1);
-    console.log(s2);
+    numbers.push(s3);
+    console.log(operators);
+    console.log(numbers);
     finalAns = opeartion();
     display.textContent = finalAns;
 })
@@ -22,8 +22,8 @@ eqaul.addEventListener("click", () => {
 AC.addEventListener("click", () => {
     display.textContent = " ";
     s1 = " ";
-    s2 = [];
-    ope1 = [];
+    numbers = [];
+    operators = [];
     s3 = "";
     finalAns = 0;
 })
@@ -33,8 +33,8 @@ for (let num of number) {
     num.addEventListener("click", () => {
         if (display.textContent == finalAns) {
             s1 = " ";
-            s2 = [];
-            ope1 = [];
+            numbers = [];
+            operators = [];
             s3 = "";
             finalAns = 0;
         }
@@ -49,17 +49,17 @@ operator.forEach((ope) => {
     ope.addEventListener("click", () => {
         if (display.textContent == finalAns) {
             s1 = " " + finalAns;
-            s2 = [];
-            ope1 = [];
+            numbers = [];
+            operators = [];
             s3 = "";
-            s2.push(s1);
+            numbers.push(s1);
             finalAns = 0;
         }
         else {
-            s2.push(s3);
+            numbers.push(s3);
             s3 = "";
         }
-        ope1.push(ope.textContent);
+        operators.push(ope.textContent);
 
 
 
@@ -72,34 +72,33 @@ operator.forEach((ope) => {
 const opeartion = () => {
     let i = 1;
     let j = 0;
-    let ans = +s2[0];
+    let ans = +numbers[0];
 
-    while (i < s2.length && j < ope1.length) {
-        if (ope1[j] == "+") {
-            ans += +s2[i];
+    while (i < numbers.length && j < operators.length) {
+        if (operators[j] == "+") {
+            ans += +numbers[i];
             i++;
             j++;
             console.log("sum", ans);
         }
-        if (ope1[j] == "-") {
-            ans -= Number(s2[i]);
+        if (operators[j] == "-") {
+            ans -= Number(numbers[i]);
             i++;
             j++;
             console.log("min", ans);
         }
-        if (ope1[j] == "÷") {
-            ans /= +s2[i];
+        if (operators[j] == "÷") {
+            ans /= +numbers[i];
             i++;
             j++;
             console.log("div", ans);
         }
-        if (ope1[j] == "x") {
-            ans *= +s2[i];
+        if (operators[j] == "x") {
+            ans *= +numbers[i];
             i++;
             j++;
             console.log("mul", ans);
         }
-        console.log("hello");
     }
     console.log(ans);
     return ans;
